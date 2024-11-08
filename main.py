420a00fc49c31e3a8c9ef8a1d3a3237937490b99
grassi-giovanni <grassi.giovanni@iisgalvanimi.edu.it>
2024-11-08 10:49:47 +0000

    commit2

--- main.py
+++ main.py
@@ -16,19 +16,28 @@ def create_connection():
         print(f"Errore nella connessione al database: {e}")
         return None
 
+mycursor.execute("""
+  CREATE TABLE IF NOT EXISTS Frutta (
+    id VARCHAR(3),
+    nome VARCHAR(255) NOT NULL,
+    colore VARCHAR(50),
+    stagione VARCHAR(50),
+    vitamina_principale VARCHAR(50),
+    calorie INT
+  );""")
 
 def inserisci_data():
     frutta_data = [
-        ('Mela', 'Rosso', 'Autunno', 'C', 52),
-        ('Banana', 'Gialla', 'Tutto l\'anno', 'K', 89),
-        ('Arancia', 'Arancione', 'Inverno', 'C', 47),
-        ('Pera', 'Verde', 'Autunno', 'C', 57),
-        ('Uva', 'Nero', 'Autunno', 'K', 69),
-        ('Fragola', 'Rosso', 'Primavera', 'C', 32),
-        ('Ananas', 'Giallo', 'Tutto l\'anno', 'C', 50),
-        ('Avocado', 'Verde', 'Tutto l\'anno', 'K', 160),
-        ('Ciliegie', 'Rosso', 'Estate', 'C', 63),
-        ('Melone', 'Arancione', 'Estate', 'A', 34)
+        ('1','Mela', 'Rosso', 'Autunno', 'C', 52),
+        ('2','Banana', 'Gialla', 'Tutto l\'anno', 'K', 89),
+        ('3','Arancia', 'Arancione', 'Inverno', 'C', 47),
+        ('4','Pera', 'Verde', 'Autunno', 'C', 57),
+        ('5','Uva', 'Nero', 'Autunno', 'K', 69),
+        ('6','Fragola', 'Rosso', 'Primavera', 'C', 32),
+        ('7','Ananas', 'Giallo', 'Tutto l\'anno', 'C', 50),
+        ('8','Avocado', 'Verde', 'Tutto l\'anno', 'K', 160),
+        ('9','Ciliegie', 'Rosso', 'Estate', 'C', 63),
+        ('10','Melone', 'Arancione', 'Estate', 'A', 34)
     ]
 
     conn = create_connection()
@@ -37,8 +46,8 @@ def inserisci_data():
         try:
 
             cursor.executemany("""
-                INSERT INTO Frutta (nome, colore, stagione, vitamina_principale, calorie)
-                VALUES (%s, %s, %s, %s, %s)
+                INSERT INTO Frutta (id, nome, colore, stagione, vitamina_principale, calorie)
+                VALUES (%s, %s, %s, %s, %s, %s)
             """, frutta_data)
             conn.commit()
             print(f"{cursor.rowcount} record inseriti nel database.")
@@ -63,3 +72,16 @@ def verifica_data():
 inserisci_data()
 
 verifica_data()
+
+def estrai_all_data():
+    conn = create_connection()
+    if conn:
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM Frutta")
+        rows = cursor.fetchall()
+        for row in rows:
+            print(row)  
+        cursor.close()
+        conn.close()
+
+estrai_all_data()
